45c44782dbb06bca1eee24f78ccf32e6c5771f4b
Erick0990 <erickgonzalezmatarrita@hotmail.com>
2026-02-15 17:46:09 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sitio-Web-Grupo-362/",  // <--- ¡ESTA LÍNEA ES LA MAGIA! AGREGA ESTO.
+  base: "/Sitio-Web-Grupo-362/",
 })
