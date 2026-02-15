1c30aba45f2b57ecc6a2883226416c3413c1e5cb
Erick0990 <erickgonzalezmatarrita@hotmail.com>
2026-02-15 16:49:51 -0600

    Set base path for Vite configuration
    Add base path configuration for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Sitio-Web-Grupo-362/",  // <--- ¡ESTA LÍNEA ES LA MAGIA! AGREGA ESTO.
 })
